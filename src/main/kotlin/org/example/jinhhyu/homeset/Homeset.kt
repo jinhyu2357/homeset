f9f4c722b9795eb3fb133edd3597404d6c64f120
jinhyu2357 <jinhyu2357@gmail.com>
2026-02-24 20:49:21 +0900

    update

---
+++ src/main/kotlin/org/example/jinhhyu/homeset/Homeset.kt
@@ -0,0 +1,66 @@
+package org.example.jinhhyu.homeset
+
+import io.papermc.paper.command.brigadier.BasicCommand
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import java.io.File
+import java.util.logging.Level
+import org.bukkit.plugin.java.JavaPlugin
+
+class Homeset : JavaPlugin() {
+    private lateinit var homeRepository: HomeRepository
+
+    override fun onEnable() {
+        if (!dataFolder.exists() && !dataFolder.mkdirs()) {
+            logger.severe("Could not create plugin data folder: ${dataFolder.absolutePath}")
+            server.pluginManager.disablePlugin(this)
+            return
+        }
+        saveDefaultConfig()
+
+        val databaseFile = File(dataFolder, "homes.db")
+        homeRepository = try {
+            HomeRepository.connect(databaseFile).also { it.initializeSchema() }
+        } catch (exception: Exception) {
+            logger.log(Level.SEVERE, "Could not initialize homes database.", exception)
+            server.pluginManager.disablePlugin(this)
+            return
+        }
+
+        val commandHandler = HomeCommandHandler(this, homeRepository)
+        try {
+            registerPaperCommand("sethome", "Set or update one of your homes.", "homeset.sethome", commandHandler)
+            registerPaperCommand("home", "Teleport to one of your homes.", "homeset.use", commandHandler)
+            registerPaperCommand("delhome", "Delete one of your homes.", "homeset.delhome", commandHandler)
+            registerPaperCommand("homes", "List your saved homes.", "homeset.use", commandHandler)
+        } catch (exception: Exception) {
+            logger.log(Level.SEVERE, "Could not register plugin commands.", exception)
+            server.pluginManager.disablePlugin(this)
+            return
+        }
+    }
+
+    override fun onDisable() {
+        if (::homeRepository.isInitialized) {
+            homeRepository.close()
+        }
+    }
+
+    private fun registerPaperCommand(
+        name: String,
+        description: String,
+        requiredPermission: String,
+        handler: HomeCommandHandler
+    ) {
+        registerCommand(name, description, object : BasicCommand {
+            override fun execute(commandSource: CommandSourceStack, args: Array<String>) {
+                handler.executeCommand(commandSource.sender, name, args)
+            }
+
+            override fun suggest(commandSource: CommandSourceStack, args: Array<String>): Collection<String> {
+                return handler.completeCommand(commandSource.sender, name, args)
+            }
+
+            override fun permission(): String = requiredPermission
+        })
+    }
+}
